2fc2e8cea7a550ae8f1ed540e9cd4037a48b255d
Evgenlyahov <89838242+Evgenlyahov@users.noreply.github.com>
2021-11-16 20:52:34 +0300

    add Example 34

---
+++ Example034/Program.cs
@@ -0,0 +1,25 @@
+﻿// 34. Написать программу замену элементов массива на противоположные
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-10, 11);
+        System.Console.Write(array[i] + " ");
+    }
+}
+
+int[] array = new int[10];
+FillArray(array);
+
+
+void ReplaceDigit(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = - array[i];
+        System.Console.Write(array[i] + " ");
+    }
+}
+System.Console.WriteLine();
+ReplaceDigit(array);

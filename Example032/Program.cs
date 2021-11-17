22ebd52512b2d4b63559c8f13023c57e96d617d5
Evgenlyahov <89838242+Evgenlyahov@users.noreply.github.com>
2021-11-17 07:14:47 +0300

    add Example 32

---
+++ Example032/Program.cs
@@ -0,0 +1,11 @@
+﻿// 32. Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+void FillArray(int[] array)
+{
+    for (byte i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+        System.Console.Write(array[i] + " ");
+    }
+}
+int[] array = new int[8];
+FillArray(array);

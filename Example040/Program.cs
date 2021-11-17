dd07dd1e5a3ff4fd9c723abec69893a0dfba2527
Evgenlyahov <89838242+Evgenlyahov@users.noreply.github.com>
2021-11-17 06:59:09 +0300

    add Example 40

---
+++ Example040/Program.cs
@@ -0,0 +1,45 @@
+﻿// 40. В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+
+void FillArray(float[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 11);
+        System.Console.Write(array[i] + " ");
+    }
+}
+
+float MaxElement(float[] array)
+{
+    float max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+    return max;
+}
+float MinElement(float[] array)
+{
+    float min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    return min;
+}
+
+float[] arr = new float[10];
+FillArray(arr);
+
+float resultMax = MaxElement(arr);
+float resultMin = MinElement(arr);
+System.Console.WriteLine();
+System.Console.WriteLine($"Максимальный элемент: {resultMax}");
+System.Console.WriteLine($"Минимальный элемент: {resultMin}");
+System.Console.WriteLine($"Разница между максимальным и минимальным элементами равна {resultMax - resultMin}");

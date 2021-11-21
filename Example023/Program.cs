e4470e52ca88cf96e2eb3ff54d926d99d8c92982
Evgenlyahov <89838242+Evgenlyahov@users.noreply.github.com>
2021-11-21 15:22:28 +0300

    add Example 23

---
+++ Example023/Program.cs
@@ -0,0 +1,36 @@
+﻿// 23. Показать таблицу квадратов чисел от 1 до N
+
+
+int[] FillArray(int sizeArray)
+{
+    int[] array = new int[sizeArray];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = i + 1;
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+}
+
+int[] array = FillArray(100);
+PrintArray(array);
+System.Console.WriteLine();
+void PrintArrayTable(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int result = array[i] * array[i];
+        if (array[i] == 11 || array[i] == 21 || array[i] == 31 || array[i] == 41 || array[i] == 51 || array[i] == 61 || array[i] == 71 || array[i] == 81 || array[i] == 91)
+        {
+            System.Console.WriteLine();
+        }
+        System.Console.WriteLine($"{array[i]} * {array[i]} = {result}");
+    }
+}
+PrintArrayTable(array);

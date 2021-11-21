dd43b7689f49fa6df57b2ad6db3d7704d2a5b996
Evgenlyahov <89838242+Evgenlyahov@users.noreply.github.com>
2021-11-21 15:23:17 +0300

    add Example 24

---
+++ Example024/Program.cs
@@ -0,0 +1,33 @@
+﻿// 24. Найти кубы чисел от 1 до N
+
+int[] FillArray(int sizeArray)
+{
+    int[] array = new int[sizeArray];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = i + 1;
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+}
+
+int[] array = FillArray(15);
+PrintArray(array);
+System.Console.WriteLine();
+
+void FofCube(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int result = array[i]*array[i]*array[i];
+        System.Console.WriteLine(result + " ");
+    }
+}
+FofCube(array);

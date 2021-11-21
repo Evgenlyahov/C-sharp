49fc1101d5527f0a458ae6f166354b35ea48122e
Evgenlyahov <89838242+Evgenlyahov@users.noreply.github.com>
2021-11-21 17:02:58 +0300

    add Glif English Language

---
+++ Glif_English_language/Program.cs
@@ -0,0 +1,15 @@
+﻿// Вывести на экран английский алфавит
+// char glif = 'a';
+// while (glif <= 'z')
+// {
+//     Console.Write(glif +" ");
+//     glif++;
+// }
+
+
+char glif = 'a';
+while(glif < 'a' + 26)
+{
+    System.Console.Write(glif + " ");
+    glif++;
+}

25c2fa0d50b72106c969dd328e4c53d28aec4b4b
Evgenlyahov <89838242+Evgenlyahov@users.noreply.github.com>
2021-11-21 16:22:33 +0300

    add Factorial

---
+++ Factorial/Program.cs
@@ -0,0 +1,16 @@
+﻿// Вычисление факториала числа
+double Factorial(int n)
+{
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+
+
+void PrintFactorial(int factorialDigit)
+{
+    for (int i = 1; i < factorialDigit; i++)
+    {
+        System.Console.WriteLine($"{i}! = {Factorial(i)}");
+    }
+}
+PrintFactorial(30);
